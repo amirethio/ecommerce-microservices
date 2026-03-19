fe56b65caabb6c16e6b75effd6dcc145190ca983
Amir Ali <amiraliyimer@gmail.com>
2026-03-19 18:18:21 +0300

    making the user database microservice

---
+++ user-service/prisma/migrations/20260319151229_init/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Cart` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `CartItem` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "RefreshToken" DROP CONSTRAINT "RefreshToken_userId_fkey";
+
+-- DropTable
+DROP TABLE "Cart";
+
+-- DropTable
+DROP TABLE "CartItem";
+
+-- DropTable
+DROP TABLE "Order";
